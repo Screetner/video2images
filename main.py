42fb032373a408815c6bc969c67a0bc0cf998a29
i am doraemon <khunball46@gmail.com>
2024-02-24 15:46:03 +0700

    new version

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ video = cv2.VideoCapture("oak-1110.mp4")
 
 # extract images from video every 1 second
 count = 0
+name_video = "VID_20240217_113307"
 while video.isOpened():
     ret, frame = video.read()
     if not ret:
         break
-    cv2.imwrite(os.path.join("./images", f"frame{count}.jpg"), frame)
+    cv2.imwrite(os.path.join(f"./{name_video}", f"{name_video}_frame{count}.jpg"), frame)
     count += 1
     video.set(cv2.CAP_PROP_POS_MSEC, count * 1000)
     print(f"Frame {count} extracted")
